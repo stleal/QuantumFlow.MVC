bb766f2d56851d9c0c1d5111527b177fc64f4aef
Samuel Teodoro Leal <Leal.Samir@Outlook.com>
2026-04-02 06:06:44 -0400

    added path to API and authentication endpoint with Login method in HomeController

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,6 +6,14 @@ namespace QuantumFlowMVC.Controllers;
 
 public class HomeController : Controller
 {
+    private readonly ILogger<HomeController> _logger;
+    private readonly IConfiguration _configuration;
+
+    public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
+    {
+        _logger = logger;
+        _configuration = configuration;
+    }
     public IActionResult Index()
     {
         return View();
@@ -14,6 +22,21 @@ public class HomeController : Controller
     public IActionResult About()
     {
         return View();
+    }
+
+    public async Task<IActionResult> Login(string username, string password)
+    {
+        if (!string.IsNullOrEmpty(username) && !string.IsNullOrEmpty(password))
+        {
+            var client = new HttpClient();
+            var apiUrl = _configuration["QuantumFlow.API:BaseUrl"] + _configuration["QuantumFlow.API:LoginEndpoint"];
+            var resp = await client.PostAsJsonAsync(apiUrl, new { Username = username, Password = password });
+            return resp.IsSuccessStatusCode ? RedirectToAction("Index", "Dashboard") : RedirectToAction("Index");
+        }
+        else
+        {
+            return RedirectToAction("Index");
+        }
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
