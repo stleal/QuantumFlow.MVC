1537beaa956b736c0e6711d3af524456655fcb5b
Samuel Teodoro Leal <Leal.Samir@Outlook.com>
2026-04-01 21:47:04 -0400

    changed Privacy page to About us page with company info and slided Dashboard link over to the left by one in place of the About link

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -11,7 +11,7 @@ public class HomeController : Controller
         return View();
     }
 
-    public IActionResult Privacy()
+    public IActionResult About()
     {
         return View();
     }
